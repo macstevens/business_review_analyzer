3ffe1c845f5834095aeb687a5badcb26bad4886d
Mac Stevens <stevensm@earthlink.net>
2024-04-23 10:04:05 -0700

    test script

---
+++ test/test_a.bat
@@ -0,0 +1,14 @@
+..\msvc\x64\Debug\business_review_analyzer_test.exe  ^
+  --start-date-time 2022-01-01T12:00:00 ^
+  --end-date-time 2024-04-20T12:00:00 ^
+  files/bbb_goldco_reviews.txt ^
+  files/consumer_affairs_goldco_reviews.htm ^
+  files/google_goldco_reviews.htm ^
+  files/trustlink_goldco_reviews.htm ^
+  files/trustpilot_goldco_reviews.htm ^
+  files/yelp_goldco__not_recommended_reviews.htm ^
+  files/yelp_goldco__recommended_reviews.txt
+  
+  
+  
+  
